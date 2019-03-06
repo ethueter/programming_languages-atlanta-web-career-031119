c5a99631c4356ae142c9e5a27b655a5a6d567a07
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 15:05:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,14 +4,10 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |language, data|
     data.each do |attribute, info|
-      if attribute == :ruby || attribute == :python || attribute == :java
-        info[:style]=[:oo] 
-        
-         binding.pry 
-        elsif attribute == :clojure || attribute == :erlang || attribute == :scala 
-        new_hash[:style]=[:functional]
-      else attribute == :javascript
-        new_hash[:style]=[:oo, :functional]
+    if new_hash == nil 
+      new_hash = data 
+    end
+    if attribute == :ruby || attribute == :
            
       end
     end
